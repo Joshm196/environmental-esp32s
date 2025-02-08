4d4b7ffdf046a11caeb7de3803aa81cdb5a4bc54
Josh M <Josh M>
2025-02-08 03:52:04 -0600

    Fix green light funciton

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 #define RED_LED 16
-#define GREEN_LED 16
+#define GREEN_LED 17
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(RED_LED,OUTPUT);
+  pinMode(GREEN_LED, OUTPUT); // new addumdum to add the pinMode Function to the code to initiate this
   Serial.begin(115200);
 
 }
